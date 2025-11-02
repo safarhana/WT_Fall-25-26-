02d186f8de442d3599988d589569cc49084e5957
safarhana <farhana.mim080@gmail.com>
2025-11-02 12:12:53 +0600

    Lab Task

---
+++ Mid/Lab Task 2/lab1_form.php
@@ -0,0 +1,63 @@
+<!Doctype html>
+
+<head>
+
+<title>Wt_k</title>
+
+</head>
+
+<body>
+
+
+<center>
+
+<h1>Clinic Patient Registration</h1>
+
+</center>
+
+Full Name:
+<input type = "text"> <br><br>
+
+Age:
+<input type="text"> <br> <br>
+
+
+Phone Number:
+<input type="text"> <br> <br>
+
+Email Address:
+<input type="email"> <br> <br>
+
+Insurance Provider:
+<input type="radio"> LifeTime
+<input type = "radio"> Yearly
+
+
+Insurance Policy Number:
+<input type="text"> <br> <br>
+
+
+<center>
+
+<h1>Additional Information</h1>
+
+</center>
+
+Username:
+<input type = "text"> <br><br>
+
+Password:
+<input type="text"> <br> <br>
+
+
+Confirm Password:
+<input type="text"> <br> <br>
+
+
+
+
+<center>
+  <input type="Register" >
+</center>
+
+</body>
