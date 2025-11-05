6d87086fec0a54d9e2f6e215f57a95bee71a59d0
safarhana <farhana.mim080@gmail.com>
2025-11-05 23:32:39 +0600

    1st form

---
+++ Mid/Lab Task 1/lab1_form.php
@@ -0,0 +1,69 @@
+<!Doctype html>
+
+<head>
+
+<title>Wt_k</title>
+
+</head>
+
+<body>
+
+
+<center>
+
+<h1>Clinic Patient Registration</h1>
+
+</center>
+
+<center>
+
+Full Name:
+<input type = "text"> <br><br>
+
+Age:
+<input type="text"> <br> <br>
+
+
+Phone Number:
+<input type="text"> <br> <br>
+
+Email Address:
+<input type="email"> <br> <br>
+
+Insurance Provider:
+<select name="Select Provider" id="Select Provider">
+  <option value="Select provider">  Select provider </option>
+  <option value="option1">Option 1</option>
+  <option value="option2">Option 2</option>
+</select>
+
+<br> <br>
+
+Insurance Policy Number:
+<input type="text"> <br> <br>
+
+</center>
+<center>
+
+<h1>Additional Information</h1>
+
+</center>
+<center>
+Username:
+<input type = "text"> <br><br>
+
+Password:
+<input type="text"> <br> <br>
+
+
+Confirm Password:
+<input type="text"> <br> <br>
+
+</center>
+
+
+<center>
+  <button>Register</button>
+</center>
+
+</body>
